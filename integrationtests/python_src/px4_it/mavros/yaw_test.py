a50d9df4bbb2bc2df01897368bfda7efcf412cf9
1apple7seeds <charliehpwiesse@gmail.com>
2021-10-14 13:30:49 +0200

    integrationtests

--- integrationtests/python_src/px4_it/mavros/yaw_test.py
+++ integrationtests/python_src/px4_it/mavros/yaw_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 #***************************************************************************
 #
-#   Copyright (c) 2015 PX4 Development Team. All rights reserved.
+#   Copyright (c) 2020 PX4 Development Team. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -31,12 +31,10 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 #***************************************************************************/
+#
+# @author Pedro Roque <padr@kth.se>
+#
 
-#
-# @author Andreas Antener <andreas@uaventure.com>
-#
-# The shebang of this file is currently Python2 because some
-# dependencies such as pymavlink don't play well with Python3 yet.
 from __future__ import division
 
 PKG = 'px4'
@@ -46,21 +44,21 @@ from geometry_msgs.msg import Quaternion, Vector3
 from mavros_msgs.msg import AttitudeTarget
 from mavros_test_common import MavrosTestCommon
 from pymavlink import mavutil
+from six.moves import xrange
 from std_msgs.msg import Header
 from threading import Thread
 from tf.transformations import quaternion_from_euler
 
 
-class MavrosOffboardAttctlTest(MavrosTestCommon):
+class MavrosOffboardYawrateTest(MavrosTestCommon):
     """
-    Tests flying in offboard control by sending attitude and thrust setpoints
-    via MAVROS.
-
-    For the test to be successful it needs to cross a certain boundary in time.
+    Tests flying in offboard control by sending a Roll Pitch Yawrate Thrust (RPYrT)
+    as attitude setpoint.
+    For the test to be successful it needs to achieve a desired yawrate and height.
     """
 
     def setUp(self):
-        super(MavrosOffboardAttctlTest, self).setUp()
+        super(MavrosOffboardYawrateTest, self).setUp()
 
         self.att = AttitudeTarget()
 
@@ -72,8 +70,12 @@ class MavrosOffboardAttctlTest(MavrosTestCommon):
         self.att_thread.daemon = True
         self.att_thread.start()
 
+        # desired yawrate target
+        self.des_yawrate = 0.1
+        self.yawrate_tol = 0.02
+
     def tearDown(self):
-        super(MavrosOffboardAttctlTest, self).tearDown()
+        super(MavrosOffboardYawrateTest, self).tearDown()
 
     #
     # Helper methods
@@ -83,10 +85,15 @@ class MavrosOffboardAttctlTest(MavrosTestCommon):
         self.att.body_rate = Vector3()
         self.att.header = Header()
         self.att.header.frame_id = "base_footprint"
-        self.att.orientation = Quaternion(*quaternion_from_euler(-0.25, 0.5,
-                                                                 0))
-        self.att.thrust = 0.7
-        self.att.type_mask = 7  # ignore body rate
+        self.att.orientation = self.local_position.pose.orientation
+
+        self.att.body_rate.x = 0
+        self.att.body_rate.y = 0
+        self.att.body_rate.z = self.des_yawrate
+
+        self.att.thrust = 0.59
+
+        self.att.type_mask = 3 # ignore roll and pitch rate
 
         while not rospy.is_shutdown():
             self.att.header.stamp = rospy.Time.now()
@@ -100,11 +107,14 @@ class MavrosOffboardAttctlTest(MavrosTestCommon):
     # Test method
     #
     def test_attctl(self):
-        """Test offboard attitude control"""
+        """Test offboard yawrate control"""
+
         # boundary to cross
-        boundary_x = 200
-        boundary_y = 100
-        boundary_z = 20
+        # Stay leveled, go up, and test yawrate
+        boundary_x = 5
+        boundary_y = 5
+        boundary_z = 10
+
 
         # make sure the simulation is ready to start the mission
         self.wait_for_topics(60)
@@ -112,23 +122,25 @@ class MavrosOffboardAttctlTest(MavrosTestCommon):
                                    10, -1)
 
         self.log_topic_vars()
-        self.set_mode("OFFBOARD", 5)
         self.set_arm(True, 5)
-
+        self.set_mode("OFFBOARD", 5)
         rospy.loginfo("run mission")
-        rospy.loginfo("attempting to cross boundary | x: {0}, y: {1}, z: {2}".
-                      format(boundary_x, boundary_y, boundary_z))
-        # does it cross expected boundaries in 'timeout' seconds?
+        rospy.loginfo("attempting to cross boundary | z: {2} , stay within x: {0}  y: {1} \n   and achieve {3} yawrate".
+                      format(boundary_x, boundary_y, boundary_z, self.des_yawrate))
+
+	    # does it cross expected boundaries in 'timeout' seconds?
         timeout = 90  # (int) seconds
         loop_freq = 2  # Hz
         rate = rospy.Rate(loop_freq)
         crossed = False
         for i in xrange(timeout * loop_freq):
-            if (self.local_position.pose.position.x > boundary_x and
-                    self.local_position.pose.position.y > boundary_y and
-                    self.local_position.pose.position.z > boundary_z):
-                rospy.loginfo("boundary crossed | seconds: {0} of {1}".format(
-                    i / loop_freq, timeout))
+            if (self.local_position.pose.position.x < boundary_x and
+	    	    self.local_position.pose.position.x > -boundary_x and
+                    self.local_position.pose.position.y < boundary_y and
+		    self.local_position.pose.position.y > -boundary_y and
+                    self.local_position.pose.position.z > boundary_z and
+		    abs(self.imu_data.angular_velocity.z - self.des_yawrate) < self.yawrate_tol):
+                rospy.loginfo("Test successful. Final altitude and yawrate achieved")
                 crossed = True
                 break
 
@@ -138,10 +150,17 @@ class MavrosOffboardAttctlTest(MavrosTestCommon):
                 self.fail(e)
 
         self.assertTrue(crossed, (
-            "took too long to cross boundaries | current position x: {0:.2f}, y: {1:.2f}, z: {2:.2f} | timeout(seconds): {3}".
+            "took too long to finish test | current position x: {0:.2f}, y: {1:.2f}, z: {2:.2f} \n " \
+	    "                             | current att qx: {3:.2f}, qy: {4:.2f}, qz: {5:.2f} qw: {6:.2f}, yr: {7:.2f}| timeout(seconds): {8}".
             format(self.local_position.pose.position.x,
                    self.local_position.pose.position.y,
-                   self.local_position.pose.position.z, timeout)))
+                   self.local_position.pose.position.z,
+		   self.imu_data.orientation.x,
+		   self.imu_data.orientation.y,
+		   self.imu_data.orientation.z,
+		   self.imu_data.orientation.w,
+		   self.imu_data.angular_velocity.z,
+		   timeout)))
 
         self.set_mode("AUTO.LAND", 5)
         self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
@@ -153,5 +172,6 @@ if __name__ == '__main__':
     import rostest
     rospy.init_node('test_node', anonymous=True)
 
-    rostest.rosrun(PKG, 'mavros_offboard_attctl_test',
-                   MavrosOffboardAttctlTest)
+    rostest.rosrun(PKG, 'mavros_offboard_yawrate_test',
+                   MavrosOffboardYawrateTest)
+
